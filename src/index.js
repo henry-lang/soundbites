99bd49aa4bf9288114e5b6bf441492ff80665e4f
henry-lang <henry.langmack@gmail.com>
2021-08-10 15:02:11 -0400

    express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+var express = require('express');
+
+var server = express();
+var port = 3000;
+
+server.listen(port, function() {
+    console.log(`Server started on port ${port}!`);
+});

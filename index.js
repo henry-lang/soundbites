a00b5609b10677d4a576306ec1129701394c217e
henry-lang <henry.langmack@gmail.com>
2021-08-10 15:50:12 -0400

    ejs

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ var path = require('path');
 var server = express();
 var port = 3000;
 
+server.set('view engine', 'ejs');
+
 server.use(express.static('pages'))
 server.use('/assets', express.static(path.join(__dirname, 'assets')))
 
-server.listen(port, function() {
+server.listen(port, () => {
     console.log(`Server started on port ${port}!`);
 });
 
-server.get('/', function(req, res) {
-    res.sendFile('pages/index.html', {root: __dirname});
+server.get('/', (req, res) => {
+    res.render('index');
+});
+
+server.get('/posts/:id', (req, res) => {
+    var id = req.params.id;
+    
+    res.render('posts', {id: id});
 });

062bb4806416097ee0a88fae8885a34d6a04d381
henry-lang <henry.langmack@gmail.com>
2021-08-10 15:16:00 -0400

    static /assets/

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var path = require('path');
 
 var server = express();
 var port = 3000;
 
+server.use(express.static('pages'))
+server.use('/assets', express.static(path.join(__dirname, 'assets')))
+
 server.listen(port, function() {
     console.log(`Server started on port ${port}!`);
+});
+
+server.get('/', function(req, res) {
+    res.sendFile('pages/index.html', {root: __dirname});
 });

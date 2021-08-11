80b1b750c752bcbbda87c3ab6e2fa6fc5b687049
henry-lang <henry.langmack@gmail.com>
2021-08-11 18:31:55 -0400

    user model

---
+++ server/models/user_model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const passwordSchema = new PasswordValidator();
+passwordSchema
+.is.min(8)
+.is.max(20)
+.has().digits(1)
+.has().not().spaces()
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    displayName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+    },
+    profilePictureURL: {
+        type: String,
+        required: true
+    },
+    author: { // Author for writing posts, other users will be able to write comments, and rate them maybe.
+        type: Boolean,
+        required: true,
+        default: false
+    }
+});
+
+userSchema.pre('validate', function(next) {
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ server/routes/user_router.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const userRouter = new express.Router();
+
+module.exports = userRouter;

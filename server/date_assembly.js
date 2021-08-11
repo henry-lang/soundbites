c32cc1ed6d555fe7ee3c7044a00196c23d9119cc
moistpotato9873 <moistpotato9873@users.noreply.github.com>
2021-08-11 12:01:56 -0400

    date assembly

---
+++ server/date_assembly.js
@@ -0,0 +1,12 @@
+function assemble() {
+    const d = new Date()
+    var day = d.getDate();
+    var month = d.getMonth() + 1;
+    var year = d.getFullYear();
+    
+    full = `${month}/${day}/${year}`
+    console.log(full)
+    return full
+}
+
+module.exports = assemble

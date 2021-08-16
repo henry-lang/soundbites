2c9e36ff380989672b50f2178322df67523541be
Henry Langmack <Henry.langmack@gmail.com>
2021-08-16 12:29:16 -0500

    r

--- server/routes/post_router.js
+++ server/routes/post_router.js
@@ -3,19 +3,19 @@ const Post = require('../models/post_model')
 
 const postRouter = express.Router()
 
+postRouter.get('/', (req, res) => {
+    res.redirect('/featured')
+})
+
 postRouter.get('/:slug', async (req, res) => {
     var slug = req.params.slug
-    var data = await Post.findOne({slug: slug}).lean()
+    var data = await Post.findOne({slug: slug})
     if(!data) {
         res.render('404')
         return
     }
 
     res.render('post', {data: data})
-})
-
-postRouter.get('/', (req, res) => {
-    res.redirect('/featured')
 })
 
 module.exports = postRouter

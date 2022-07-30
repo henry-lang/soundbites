725c27b0cfb0cb3830c5c820dbfb3c2de9e1a5b2
Henry Langmack <henry.langmack@gmail.com>
2022-07-30 11:32:56 -0400

    bump astro version to rc3 and add sitemap

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
     site: 'https://soundbites.xyz',
+    integrations: [sitemap()],
 })
